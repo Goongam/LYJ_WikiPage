0a83357138ad046868e50dac0890973632cbda75
Goongam <dlwjddn341@naver.com>
2024-02-16 01:02:50 +0900

    feat : 메인페이지 높이 조절

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home({ searchParams }: Props) {
   const page = Number(searchParams?.page);
 
   return (
-    <div className="w-full flex flex-col p-5 gap-10">
+    <div className="w-full flex flex-col p-5 gap-10 h-full">
       <div className="w-full border rounded-md h-32 p-2">
         <h2 className="text-5xl font-extrabold">코딩위키</h2>
         <p className="mt-2">우리 모두가 만들어가는 자유 백과사전</p>
