0276aa2257a8633ba922bc2d6e00acb6da5f686b
Goongam <dlwjddn341@naver.com>
2024-02-15 20:24:59 +0900

    feat : 페이지네이션 연결 및 해당 페이지 위키글 가져오기

--- src/components/WikiList.tsx
+++ src/components/WikiList.tsx
@@ -1,14 +1,23 @@
-import { getAllArticles } from "@/service/article";
+import {
+  getAllArticles,
+  getArticles,
+  getArticlesCount,
+} from "@/service/article";
 import WikiListTitle from "./WikiListTitle";
-
-export default async function WikiList() {
-  const articles = await getAllArticles();
-
+import PageNation from "./ui/PageNation";
+import { PAGE_SHOW_COUNT } from "@/constants";
+interface Props {
+  page?: number;
+}
+export default async function WikiList({ page = 1 }: Props) {
+  const articles = await getArticles(page);
+  const maxPage = Math.ceil((await getArticlesCount()) / PAGE_SHOW_COUNT);
   return (
     <section className="flex flex-col gap-2">
       {articles.map(({ title }) => (
         <WikiListTitle title={title} key={title} />
       ))}
+      <PageNation maxPage={maxPage} showPage={5} currentPage={page} />
     </section>
   );
 }
