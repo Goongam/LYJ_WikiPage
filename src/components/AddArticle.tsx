e2c05cbdf133caee06b7d1a1e20796e2a0694ba3
Goongam <dlwjddn341@naver.com>
2024-02-16 03:45:26 +0900

    refactor : 업로드 핵심코드 분리

--- src/components/AddArticle.tsx
+++ src/components/AddArticle.tsx
@@ -1,87 +1,16 @@
 "use client";
-import { useMutate } from "@/hooks/useFetch";
-import { useInput } from "@/hooks/useInput";
-import { addArticle } from "@/service/article";
-import { validArticle } from "@/validations/newArticle";
-import { redirect, useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+
+import UpdateArticleForm from "./UpdateArticleForm";
 
 export default function AddArticle() {
-  const {
-    value: titleValue,
-    onchange: titleChange,
-    error: titleError,
-    showError: showTitleError,
-  } = useInput("");
-  const {
-    value: contentValue,
-    onchange: contentChange,
-    error: contentError,
-    showError: showContentError,
-  } = useInput("");
-
-  const { loading, mutate, serverError } = useMutate(
-    () =>
-      fetch("/api/new", {
-        method: "POST",
-        body: JSON.stringify({
-          title: titleValue,
-          content: contentValue,
-        }),
+  const newArticle = (title: string, content: string) =>
+    fetch("/api/new", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        content,
       }),
-    {
-      onSuccess: () => router.push(`/wiki/${titleValue}`),
-    }
-  );
+    });
 
-  const router = useRouter();
-
-  const submit = (e: FormEvent) => {
-    e.preventDefault();
-
-    if (!validArticle.title(titleValue)) return showTitleError();
-    if (!validArticle.content(contentValue)) return showContentError();
-
-    mutate();
-  };
-
-  return (
-    <section className="w-full h-full flex flex-col items-center justify-center p-2 gap-2">
-      {serverError && (
-        <p className="w-full text-center h-20 flex items-center justify-center bg-red-400 rounded-md text-white font-bold max-w-screen-md">
-          {serverError}
-        </p>
-      )}
-      <form
-        onSubmit={submit}
-        className="flex flex-col w-full h-full max-w-screen-md gap-5"
-      >
-        <input
-          type="text"
-          value={titleValue}
-          onChange={titleChange}
-          placeholder="제목을 입력하세요"
-          className={`${
-            titleError && "border border-red-500"
-          } outline-none border p-2 rounded-md`}
-        />
-        <textarea
-          value={contentValue}
-          onChange={contentChange}
-          placeholder="내용을 입력하세요"
-          className={`${
-            contentError && "border border-red-500"
-          } outline-none flex-1 resize-none border p-2 rounded-md`}
-        />
-        <button className="bg-green-400 rounded-lg p-2 text-slate-950 font-bold">
-          작성
-        </button>
-      </form>
-      {loading && (
-        <div className="w-full h-[100vh] fixed top-0 bg-slate-400/50 flex items-center justify-center font-extrabold text-3xl">
-          업로드 중...
-        </div>
-      )}
-    </section>
-  );
+  return <UpdateArticleForm submit={newArticle} type="new" />;
 }
---
+++ src/components/UpdateArticleForm.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { useMutate } from "@/hooks/useFetch";
+import { useInput } from "@/hooks/useInput";
+import { Article, addArticle } from "@/service/article";
+import { validArticle } from "@/validations/newArticle";
+import { redirect, useRouter } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+import UpdateButton from "./ui/UpdateButton";
+import Uploading from "./ui/Uploading";
+
+export type UploadType = "new" | "edit";
+
+interface Props {
+  type: UploadType;
+  submit: (title: string, content: string) => Promise<Response>;
+  initData?: Omit<Article, "date">;
+}
+
+export default function UpdateArticleForm({
+  type,
+  submit,
+  initData = { title: "", content: "" },
+}: Props) {
+  const {
+    value: titleValue,
+    onchange: titleChange,
+    error: titleError,
+    showError: showTitleError,
+  } = useInput(initData.title);
+  const {
+    value: contentValue,
+    onchange: contentChange,
+    error: contentError,
+    showError: showContentError,
+  } = useInput(initData.content);
+
+  const router = useRouter();
+
+  // 업로드 api 커스텀 훅
+  const { loading, mutate, serverError } = useMutate(
+    () => submit(titleValue, contentValue),
+    {
+      onSuccess: () => router.push(`/wiki/${titleValue}`),
+    }
+  );
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    // 텍스트 검증
+    if (!validArticle.title(titleValue)) return showTitleError();
+    if (!validArticle.content(contentValue)) return showContentError();
+
+    mutate();
+  };
+
+  return (
+    <section className="w-full h-full flex flex-col items-center justify-center p-2 gap-2">
+      {serverError && (
+        <p className="w-full text-center h-20 flex items-center justify-center bg-red-400 rounded-md text-white font-bold max-w-screen-md">
+          {serverError}
+        </p>
+      )}
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col w-full h-full max-w-screen-md gap-5"
+      >
+        <input
+          type="text"
+          value={titleValue}
+          onChange={titleChange}
+          placeholder="제목을 입력하세요"
+          className={`${
+            titleError && "border border-red-500"
+          } outline-none border p-2 rounded-md`}
+          disabled={type === "edit"} //타입이 edit일 경우 수정불가
+        />
+        <textarea
+          value={contentValue}
+          onChange={contentChange}
+          placeholder="내용을 입력하세요"
+          className={`${
+            contentError && "border border-red-500"
+          } outline-none flex-1 resize-none border p-2 rounded-md`}
+        />
+        <UpdateButton type={type} />
+      </form>
+      <Uploading loading={loading} />
+    </section>
+  );
+}
