13661ab891bcd6ca90677a38ba9b8952ba6bd0fa
Goongam <dlwjddn341@naver.com>
2024-02-15 20:25:55 +0900

    feat : 위키 리스트 컴포넌트 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import Header from "@/components/Header";
 import WikiList from "@/components/WikiList";
 
-export default function Home() {
+interface Props {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default function Home({ searchParams }: Props) {
+  const page = Number(searchParams?.page);
+
   return (
     <div className="w-full flex flex-col p-5 gap-10">
       <div className="w-full border rounded-md h-32 p-2">
         <h2 className="text-5xl font-extrabold">코딩위키</h2>
         <p className="mt-2">우리 모두가 만들어가는 자유 백과사전</p>
       </div>
-      <WikiList />
+      <WikiList page={isNaN(page) ? 1 : page} />
     </div>
   );
 }
