73c77ecbb05e6e8c6b98c1092d028c2a51388b3f
Goongam <dlwjddn341@naver.com>
2024-02-15 19:53:46 +0900

    feat : 메인 페이지 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,13 @@ import Header from "@/components/Header";
 import WikiList from "@/components/WikiList";
 
 export default function Home() {
-  return <WikiList />;
+  return (
+    <div className="w-full flex flex-col p-5 gap-10">
+      <div className="w-full border rounded-md h-32 p-2">
+        <h2 className="text-5xl font-extrabold">코딩위키</h2>
+        <p className="mt-2">우리 모두가 만들어가는 자유 백과사전</p>
+      </div>
+      <WikiList />
+    </div>
+  );
 }
