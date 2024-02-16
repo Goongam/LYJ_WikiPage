8670f36cc1fc285f2e105ac4259cca6cfbd160bf
Goongam <dlwjddn341@naver.com>
2024-02-17 03:15:26 +0900

    refactor : 현재 페이지 변수 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,15 @@ interface Props {
 export default function Home({ searchParams }: Props) {
   const page = Number(searchParams?.page);
 
+  const currentPage = isNaN(page) ? 1 : page;
+
   return (
     <div className="w-full flex flex-col p-5 gap-10 h-full">
       <div className="w-full border rounded-md h-32 p-2">
         <h2 className="text-5xl font-extrabold">코딩위키</h2>
         <p className="mt-2">지식 저장소 코딩위키입니다</p>
       </div>
-      <ArticleList page={isNaN(page) ? 1 : page} />
+      <ArticleList page={currentPage} />
     </div>
   );
 }
