c852f6628112106abe6c7356f01029f8c3b82419
Goongam <dlwjddn341@naver.com>
2024-02-17 04:32:54 +0900

    feat : 페이지네이션 상수

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,10 +1,11 @@
 import { getArticles, getArticlesCount } from "@/service/article";
 import ArticleListTitle from "./ArticleListTitle";
 import PageNation from "./ui/PageNation";
-import { PAGE_SHOW_COUNT } from "@/constants";
+import { PAGELIST_SHOW_COUNT, PAGE_SHOW_COUNT } from "@/constants";
 interface Props {
   page?: number;
 }
+
 export default async function ArticleList({ page = 1 }: Props) {
   const articles = await getArticles(page);
   const maxPage = Math.ceil((await getArticlesCount()) / PAGE_SHOW_COUNT);
@@ -15,7 +16,11 @@ export default async function ArticleList({ page = 1 }: Props) {
         <ArticleListTitle title={title} key={title} />
       ))}
       <div className="mt-auto">
-        <PageNation maxPage={maxPage} showPage={5} currentPage={page} />
+        <PageNation
+          maxPage={maxPage}
+          showPage={PAGELIST_SHOW_COUNT}
+          currentPage={page}
+        />
       </div>
     </section>
   );
