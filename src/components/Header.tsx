2450ed4b723017e3b75b829b575577bc49a3d513
Goongam <dlwjddn341@naver.com>
2024-02-17 15:11:01 +0900

    fix : 홈이동 시 최신상태 미반영 문제 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default async function Header() {
   return (
     <header className="w-full min-h-14 bg-black flex justify-between items-center text-white px-3">
-      <Link href={"/"} className="font-extrabold text-3xl">
+      <a href="/" className="font-extrabold text-3xl">
         CODING WIKI
-      </Link>
+      </a>
       <Link href={"/new"} className="font-bold">
         추가
       </Link>
